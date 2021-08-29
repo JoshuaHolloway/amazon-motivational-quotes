138e43f4e07dc2b9999482af28790b318f28e8cb
JoshuaHolloway <jhollow6@asu.edu>
2021-08-29 06:22:11 -0500

    POST request to write quote to database

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,16 +1,67 @@
+import { useEffect } from 'react';
+
+import useHttp from '../hooks/use-http';
+import { getAllQuotes } from '../lib/api';
+
 import QuoteList from '../components/quotes/QuoteList';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
 
 // ==============================================
 
-const DUMMY_QUOTES = [
-  { id: 'q1', author: 'Josh', text: 'this is the flipping text!' },
-  { id: 'q2', author: 'Joshua', text: 'this is even more flipping text!' },
-];
+// const DUMMY_QUOTES = [
+//   { id: 'q1', author: 'Josh', text: 'this is the flipping text!' },
+//   { id: 'q2', author: 'Joshua', text: 'this is even more flipping text!' },
+// ];
 
 // ==============================================
 
 const AllQuotes = () => {
-  return <QuoteList quotes={DUMMY_QUOTES} />;
+  // --------------------------------------------
+
+  const startWithPending = true;
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes, // alias (data -> loadedQuotes)
+    error,
+  } = useHttp(getAllQuotes, startWithPending);
+
+  // --------------------------------------------
+
+  // Send request when the component loads:
+  //  -dependency list: []
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  // --------------------------------------------
+
+  if (status === 'pending') {
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  // --------------------------------------------
+
+  if (error) {
+    return <p className='centered focused'>{error}</p>;
+  }
+
+  // --------------------------------------------
+
+  // -no error but also no quotes:
+  if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
+  // --------------------------------------------
+
+  // return <QuoteList quotes={DUMMY_QUOTES} />;
+  return <QuoteList quotes={loadedQuotes} />;
 };
 
 // ==============================================
