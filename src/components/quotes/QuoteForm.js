774af65c6b280387a7392614826ffbf6e9396446
JoshuaHolloway <jhollow6@asu.edu>
2021-08-29 02:55:40 -0500

    prevent unwanted route transitions

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,4 +1,5 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import { Prompt } from 'react-router-dom';
 
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
@@ -7,6 +8,10 @@ import classes from './QuoteForm.module.css';
 // ==============================================
 
 const QuoteForm = (props) => {
+  // --------------------------------------------
+
+  const [isEntering, setIsEntering] = useState(false);
+
   // --------------------------------------------
 
   const authorInputRef = useRef();
@@ -27,28 +32,63 @@ const QuoteForm = (props) => {
 
   // --------------------------------------------
 
-  return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+  const formFocusHandler = () => {
+    console.log('FOCUS');
 
-        <div className={classes.control}>
-          <label htmlFor='author'>Author</label>
-          <input type='text' id='author' ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='text'>Text</label>
-          <textarea id='text' rows='5' ref={textInputRef}></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
-        </div>
-      </form>
-    </Card>
+    setIsEntering(true);
+  };
+
+  // --------------------------------------------
+
+  const formBlurHandler = () => {
+    console.log('BLUR');
+  };
+
+  // --------------------------------------------
+
+  const finishEnteringHandler = () => {
+    setIsEntering(false);
+  };
+
+  // --------------------------------------------
+
+  return (
+    <>
+      <Prompt
+        when={isEntering}
+        message={(location) => {
+          return 'Are you sure you want to leave? All your entered data will be lost.';
+        }}
+      />
+      <Card>
+        <form
+          onFocus={formFocusHandler}
+          onBlur={formBlurHandler}
+          className={classes.form}
+          onSubmit={submitFormHandler}
+        >
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
+
+          <div className={classes.control}>
+            <label htmlFor='author'>Author</label>
+            <input type='text' id='author' ref={authorInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor='text'>Text</label>
+            <textarea id='text' rows='5' ref={textInputRef}></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button onClick={finishEnteringHandler} className='btn'>
+              Add Quote
+            </button>
+          </div>
+        </form>
+      </Card>
+    </>
   );
 
   // --------------------------------------------
