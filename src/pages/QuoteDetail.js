bc771b8e9016a1ff508530098be8bb6ce530d5fe
JoshuaHolloway <jhollow6@asu.edu>
2021-08-29 23:45:01 -0500

    sending and getting quote data via http

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,16 +1,20 @@
+import { useEffect } from 'react';
+
 import { useParams, Route, Link, useRouteMatch } from 'react-router-dom';
 
-// ==============================================
+import useHttp from '../hooks/use-http';
+import { getSingleQuote } from '../lib/api';
 
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
 
 // ==============================================
 
-const DUMMY_QUOTES = [
-  { id: 'q1', author: 'Josh', text: 'this is the flipping text!' },
-  { id: 'q2', author: 'Joshua', text: 'this is even more flipping text!' },
-];
+// const DUMMY_QUOTES = [
+//   { id: 'q1', author: 'Josh', text: 'this is the flipping text!' },
+//   { id: 'q2', author: 'Joshua', text: 'this is even more flipping text!' },
+// ];
 
 // ==============================================
 
@@ -19,14 +23,45 @@ const QuoteDetail = () => {
 
   const match = useRouteMatch();
   const params = useParams();
+  // console.log('match: ', match);
 
-  console.log('match: ', match);
+  const { quoteId } = params;
 
   // --------------------------------------------
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
 
-  if (!quote) {
+  // --------------------------------------------
+
+  // const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  // --------------------------------------------
+
+  if (status === 'pending') {
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className='centered'>{error}</p>;
+  }
+
+  // -We will always have a loaded quote.
+  // -When the loaded quote does not have text
+  //  then loadedQuoate.text is undefined or falsey
+  if (!loadedQuote.text) {
     return <p>No quote found!</p>;
   }
 
@@ -35,7 +70,7 @@ const QuoteDetail = () => {
   return (
     <>
       {/* <p>{params.quoteId}</p> */}
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
 
       {/* <Route path={`/quotes/${params.quoteId}`} exact> */}
       <Route path={match.path} exact>
